f82fb2321d0e0b335fca233403e5fe940003a53d
margaiwangara <margaiwangara@gmail.com>
2019-09-17 12:21:28 +0300

    integrated instant page for faster page loads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
         @yield('styles')
     </head>
 
-    <body class="has-navbar-fixed-top">
+    <body>
         <div id="app">
 
             <!-- Partials -->
@@ -36,6 +36,7 @@
          <!-- Scripts -->
 
          <script src="{{ asset('js/scripts.js') }}"></script>
+         <script src="//instant.page/2.0.0" type="module" defer integrity="sha384-D7B5eODAUd397+f4zNFAVlnDNDtO1ppV8rPnfygILQXhqu3cUndgHvlcJR2Bhig8"></script>
          @yield('scripts')
     </body>
 
