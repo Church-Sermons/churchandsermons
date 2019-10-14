adb0ea3706b06c1fb119e7cd89d31a4de2c66979
margaiwangara <margaiwangara@gmail.com>
2019-10-14 15:25:59 +0300

    mediables table migrations solution

--- database/migrations/2019_09_04_142907_create_mediable_tables.php
+++ database/migrations/2019_09_04_142907_create_mediable_tables.php
@@ -13,44 +13,41 @@ class CreateMediableTables extends Migration
      */
     public function up()
     {
-        Schema::create('multimedia', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('disk', 32);
-            $table->string('directory');
-            $table->string('filename');
-            $table->string('extension', 32);
-            $table->string('mime_type', 128);
-            $table->string('aggregate_type', 32);
-            $table->integer('size')->unsigned();
-            $table->timestamps();
-
-            $table->index(['disk', 'directory']);
-            $table->unique(['disk', 'directory', 'filename', 'extension']);
-            $table->index('aggregate_type');
-        });
-
-        Schema::create('mediables', function (Blueprint $table) {
-            $table->integer('media_id')->unsigned();
-            $table->string('mediable_type');
-            $table->integer('mediable_id')->unsigned();
-            $table->string('tag');
-            $table->integer('order')->unsigned();
-
-            $table->primary([
-                'media_id',
-                'mediable_type',
-                'mediable_id',
-                'tag'
-            ]);
-            $table->index(['mediable_id', 'mediable_type']);
-            $table->index('tag');
-            $table->index('order');
-            $table
-                ->foreign('media_id')
-                ->references('id')
-                ->on('multimedia')
-                ->onDelete('cascade');
-        });
+        // Schema::create('multimedia', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->string('disk', 32);
+        //     $table->string('directory');
+        //     $table->string('filename');
+        //     $table->string('extension', 32);
+        //     $table->string('mime_type', 128);
+        //     $table->string('aggregate_type', 32);
+        //     $table->integer('size')->unsigned();
+        //     $table->timestamps();
+        //     $table->index(['disk', 'directory']);
+        //     $table->unique(['disk', 'directory', 'filename', 'extension']);
+        //     $table->index('aggregate_type');
+        // });
+        // Schema::create('mediables', function (Blueprint $table) {
+        //     $table->integer('media_id')->unsigned();
+        //     $table->string('mediable_type');
+        //     $table->integer('mediable_id')->unsigned();
+        //     $table->string('tag');
+        //     $table->integer('order')->unsigned();
+        //     $table->primary([
+        //         'media_id',
+        //         'mediable_type',
+        //         'mediable_id',
+        //         'tag'
+        //     ]);
+        //     $table->index(['mediable_id', 'mediable_type']);
+        //     $table->index('tag');
+        //     $table->index('order');
+        //     $table
+        //         ->foreign('media_id')
+        //         ->references('id')
+        //         ->on('multimedia')
+        //         ->onDelete('cascade');
+        // });
     }
 
     /**
@@ -60,7 +57,7 @@ class CreateMediableTables extends Migration
      */
     public function down()
     {
-        Schema::drop('mediables');
-        Schema::drop('multimedia');
+        // Schema::drop('mediables');
+        // Schema::drop('multimedia');
     }
 }
